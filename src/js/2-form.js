21c77dd8367d1368c223d6a464dff3e977d21b39
Daria2608 <dariya.pitra@gmail.com>
2024-02-12 01:08:37 +0300

    goit-js-hw-09-1

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,45 @@
 console.log("form");
+
+const form = document.querySelector('form')
+const input = document.querySelector('input')
+const message = document.querySelector('textarea')
+const btn = document.querySelector('button')
+const keyOfStorage = "feedback-form-state";
+
+
+function readForm (form) {
+  const userEmail = input.value.trim();
+  const userMessage = message.value.trim();  
+  return {
+        userEmail,
+        userMessage
+    }
+}
+
+form.addEventListener('input', (event) => {
+    event.preventDefault();
+    const data = readForm(event.currentTarget);
+    const jsonData = JSON.stringify(data);
+    localStorage.setItem(keyOfStorage, jsonData)
+})
+
+
+const rowData = localStorage.getItem(keyOfStorage);
+if (rowData) {
+    const data = JSON.parse(rowData)
+    input.value = data.userEmail || "";
+    message.value = data.userMessage || "";
+}
+
+form.addEventListener('submit', userSubmit)
+
+function userSubmit(event) {
+    event.preventDefault()
+    if (input.value.trim() && message.value.trim()) {
+        console.log({
+            email: input.value.trim(),
+            message: message.value.trim(),
+        })
+    }
+    localStorage.removeItem(keyOfStorage)
+}
